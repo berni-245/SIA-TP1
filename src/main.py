aa7bf713789c82b9f4a248c3d5d3a538938fbb32
berni <bernizapico@gmail.com>
2025-03-25 22:26:29 -0300

    chore: added basic main.py test file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
-from typing import List
+from typing import List, Tuple
+from anytree import Node
 from sokoban import Sokoban, SokobanBoard, SokobanFieldType, SokobanAction
+from algorithms import search_algorithm, bfs, dfs
 
 board_matrix: List[List[SokobanFieldType]] = [
     [SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL],
@@ -8,34 +10,48 @@ board_matrix: List[List[SokobanFieldType]] = [
     [SokobanFieldType.WALL, SokobanFieldType.AIR, SokobanFieldType.AIR, SokobanFieldType.GOAL, SokobanFieldType.WALL],
     [SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL]
 ]
+
+goal_matrix1: List[List[SokobanFieldType]] = [
+    [SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL],
+    [SokobanFieldType.WALL, SokobanFieldType.AIR, SokobanFieldType.AIR, SokobanFieldType.AIR, SokobanFieldType.WALL],
+    [SokobanFieldType.WALL, SokobanFieldType.AIR, SokobanFieldType.AIR, SokobanFieldType.PLAYER, SokobanFieldType.WALL],
+    [SokobanFieldType.WALL, SokobanFieldType.AIR, SokobanFieldType.AIR, SokobanFieldType.BOX_ON_GOAL, SokobanFieldType.WALL],
+    [SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL]
+]
+
+goal_matrix2: List[List[SokobanFieldType]] = [
+    [SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL],
+    [SokobanFieldType.WALL, SokobanFieldType.AIR, SokobanFieldType.AIR, SokobanFieldType.AIR, SokobanFieldType.WALL],
+    [SokobanFieldType.WALL, SokobanFieldType.AIR, SokobanFieldType.AIR, SokobanFieldType.AIR, SokobanFieldType.WALL],
+    [SokobanFieldType.WALL, SokobanFieldType.AIR, SokobanFieldType.PLAYER, SokobanFieldType.BOX_ON_GOAL, SokobanFieldType.WALL],
+    [SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL, SokobanFieldType.WALL]
+]
     
 board: SokobanBoard = SokobanBoard(board_matrix)
+goal1: SokobanBoard = SokobanBoard(goal_matrix1)
+goal2: SokobanBoard = SokobanBoard(goal_matrix2)
 sokoban: Sokoban = Sokoban([board])
 
-print(sokoban.get_board()
-      )
-print(SokobanAction.DOWN)
-sokoban.execute_action(SokobanAction.DOWN)
-print(sokoban.get_board())
+result: Tuple[Node] = search_algorithm(
+    board,
+    [goal1, goal2],
+    [SokobanAction.UP, SokobanAction.DOWN, SokobanAction.LEFT, SokobanAction.RIGHT],
+    bfs
+)
 
-print(SokobanAction.LEFT)
-sokoban.execute_action(SokobanAction.LEFT)
-print(sokoban.get_board())
+print("BFS:")
+for i, node in enumerate(result):
+    print(f"step: {i}")
+    print(node.name, "\n")
 
-print(SokobanAction.RIGHT)
-sokoban.execute_action(SokobanAction.RIGHT)
-print(sokoban.get_board())
+result: Tuple[Node] = search_algorithm(
+    board,
+    [goal1, goal2],
+    [SokobanAction.UP, SokobanAction.DOWN, SokobanAction.LEFT, SokobanAction.RIGHT],
+    dfs
+)
 
-print(SokobanAction.UP)
-sokoban.execute_action(SokobanAction.UP)
-print(sokoban.get_board())
-
-print(SokobanAction.RIGHT)
-sokoban.execute_action(SokobanAction.RIGHT)
-print(sokoban.get_board())
-
-print(SokobanAction.DOWN)
-sokoban.execute_action(SokobanAction.DOWN)
-print(sokoban.get_board())
-
-
+print("DFS:")
+for i, node in enumerate(result):
+    print(f"step: {i}")
+    print(node.name, "\n")
