ba9c28ed569299e69fac258e1e1620c4291b5a0f
Franco Morroni <fmorroni99@gmail.com>
2025-03-27 06:56:50 -0300

    feat: heuristics

--- src/main.py
+++ src/main.py
@@ -1,18 +1,85 @@
 from typing import Tuple
 from anytree import Node
-from sokoban import SokobanAction
-from boards import board1, board2
-from algorithms import search_algorithm, dfs, bfs
+from sokoban import SokobanAction, SokobanBoard
+# from boards import board1, board2
+from algorithms import a_star, heuristic_manhatan, search_algorithm, dfs, bfs, greedy
 import sys
 sys.setrecursionlimit(10000)
-    
+
+# board_str = """
+# ####  ######
+# #  #  #   g#
+# #  ####    #
+# #    g    b#
+# # p    b   #
+# #        ###
+# ##########      
+# """
+
+# board_str = """
+# ####
+# #g #
+# #  ##
+# # b #
+# # p #
+# #  #
+# ####
+# """
+
+# board_str = """
+# #############
+# #           #
+# #  b        #
+# #           #
+# #     p     #
+# #          g#
+# #############
+# """
+
+board_str = """
+##########################
+#######g##################
+#######g#######  #########
+###         ##   #########
+##  # # # #         g#####
+#  ##     ##  # ##########
+# ##  # #  ## # ##########
+#     bpb     #   b ######
+####  ###  ####     ######
+##########################
+"""
+
+# board_str = """
+# ###############
+# #######g#######
+# #######g#######
+# ###         ###
+# ##  # # # #  ##
+# #  ##     ##  #
+# # ##  # #  ## #
+# #     bpb     #
+# ####  ###  ####
+# ###############
+# """
+
+board = SokobanBoard.board_builder(board_str)
+
+import time
+
+start_time = time.time()
+
 result: Tuple[Node, ...] = search_algorithm(
-    board2,
+    board,
     [SokobanAction.UP, SokobanAction.DOWN, SokobanAction.LEFT, SokobanAction.RIGHT],
-    bfs
+    a_star
 )
 
-print("BFS:")
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print("Greedy 2:")
 for i, node in enumerate(result):
     print(f"step: {i}")
     print(node.name, "\n")
+
+print(f"Elapsed time: {elapsed_time:.4f} seconds")
